16e03fca2850ca1407ec4f5cbf2afb5c0b184771
srikanthkadapala <srikanthkadapala@gmail.com>
2020-04-01 13:05:15 +0530

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Practice_Project_1',
+    version='',
+    packages=['pkg1', 'pkg2'],
+    url='',
+    license='',
+    author='abhi',
+    author_email='',
+    description=''
+)
